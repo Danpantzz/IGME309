ee2dd03eae9ce88fcaeeef654e83a2e1a0907502
Danpantzz <danpantz@gmail.com>
2023-03-05 22:42:35 -0500

    finished E07

--- E07 - Gimbal Removal/AppClass.cpp
+++ E07 - Gimbal Removal/AppClass.cpp
@@ -38,18 +38,29 @@ void Application::Display(void)
 	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
 	m_m4Model = glm::rotate(m_m4Model, glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
 
-	quaternion q1 = glm::angleAxis(glm::radians(m_v3Rotation.x), vector3(1.0f, 0.0f, 0.0f));
-	quaternion q2 = glm::angleAxis(glm::radians(m_v3Rotation.y), vector3(0.0f, 1.0f, 0.0f));
-	quaternion q3 = glm::angleAxis(glm::radians(m_v3Rotation.z), vector3(0.0f, 0.0f, 1.0f));
-	quaternion q4 = q1 * q2;
-	q4 = glm::cross(q4, q3); // interchangeable with above
+	// not sure if this is the right way, but it is the only way I could figure out how to get everything to work, despite not using the 4th quaternion
+	quaternion q1 = glm::angleAxis(glm::radians(1.0f), vector3(1.0f, 0.0f, 0.0f));
+	quaternion q2 = glm::angleAxis(glm::radians(1.0f), vector3(0.0f, 1.0f, 0.0f));
+	quaternion q3 = glm::angleAxis(glm::radians(1.0f), vector3(0.0f, 0.0f, 1.0f));
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X)) {
+		m_qOrientation = m_qOrientation * q1;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Y)) {
+		m_qOrientation = m_qOrientation * q2;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
+		m_qOrientation = m_qOrientation * q3;
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::R)) {
+		m_qOrientation = quaternion();
+	}
 
 	/*
 	* The following line was replaced by the model manager so we can see a model instead of a cone
 	*/
 	//m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_m4Model));
-	m_pModelMngr->AddModelToRenderList(m_sSteve,ToMatrix4(q4));
-
+	m_pModelMngr->AddModelToRenderList(m_sSteve,ToMatrix4(m_qOrientation));
 
 	// draw a skybox
 	m_pModelMngr->AddSkyboxToRenderList();
